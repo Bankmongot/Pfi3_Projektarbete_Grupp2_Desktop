936ec4b4a0fa8c786bfa449514ce3dbaf31ac06d
Emelie Larsson <emelie-larsson3@live.se>
2015-05-27 09:58:57 +0200

    hej

--- ScreenSwing/src/se/mah/k3/Themes/BottleTheme.java
+++ ScreenSwing/src/se/mah/k3/Themes/BottleTheme.java
@@ -1,4 +1,4 @@
-package se.mah.k3;
+package se.mah.k3.Themes;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -7,31 +7,32 @@ import se.mah.k3.ThemeInterface;
 import java.awt.Color;
 import javax.swing.ImageIcon;
 
+
+
   public class BottleTheme extends JPanel implements ThemeInterface {
        FirebaseData fbData;
        JLabel myLabel;
-       private final JPanel panel = new JPanel();
-       panel.setBounds
-  
+       
+       
        
        public BottleTheme(){
     	   
-  	setBackground(Color.WHITE);
-   setLayout(null);
-           panel.setBounds(0, 0, 0, 0);
-           add(panel);
+  	setBackground(Color.GREEN);
+  	setLayout(null);
 
-           myLabel = new JLabel("New label");
-   myLabel.setBounds(118, 26, 207, 16);
+   myLabel = new JLabel("New label");
+   myLabel.setBounds(128, 30, 207, 16);
    myLabel.setText("The question will appear here!");
    add(myLabel);
    
    JLabel lblNewLabel = new JLabel("New label");
-   lblNewLabel.setIcon(new ImageIcon(se.mah.k3.BottleTheme.class.getResource("/images/Flaska1.png")));
-   lblNewLabel.setBounds(196, 172, 366, 197);
+   lblNewLabel.setIcon(new ImageIcon(BottleTheme.class.getResource("/images/Flaska1.png")));
+   lblNewLabel.setBounds(4, 11, 331, 343);
    add(lblNewLabel);
    }
 
+       
+       
    @Override
 public void updateData(FirebaseData data) {
    // TODO Auto-generated method stub
