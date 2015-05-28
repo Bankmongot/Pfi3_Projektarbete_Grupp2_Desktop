07ced5c8a43a1b837e121ab29585379c48646eec
Mcni91 <mcni.91@gmail.com>
2015-05-28 09:43:16 +0200

    VerticalBoxes text align fixad

--- ScreenSwing/src/se/mah/k3/Themes/VerticalBoxes.java
+++ ScreenSwing/src/se/mah/k3/Themes/VerticalBoxes.java
@@ -2,8 +2,11 @@ package se.mah.k3.Themes;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -27,24 +30,24 @@ public class VerticalBoxes extends JPanel implements ThemeInterface {
 	int yFloor = 625;
 	int graphHeight = 600;
 	int xAlign = 512; //Where all the boxes should be x-wise
-	
-	
+
+
 	JLabel myLabel;
 	GraphBox box;
 	List<GraphBox> boxes = new ArrayList<GraphBox>();
 
 	public VerticalBoxes(){
-		
+
 		//percent = Helpers.calcPercent(fbData.getInData());
-		
+
 		setLayout(null);
 		setPreferredSize(new Dimension(1080,560));
 		setMinimumSize(new Dimension(1080,560));
-		
+
 		boxes.add(new GraphBox(Color.blue));
 		boxes.add(new GraphBox(Color.orange));
 		boxes.add(new GraphBox(Color.yellow));
-		
+
 		myLabel = new JLabel("New label");
 		myLabel.setBounds(161, 224, 207, 16);
 		myLabel.setText("The question will appear here!");
@@ -53,67 +56,65 @@ public class VerticalBoxes extends JPanel implements ThemeInterface {
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
+
 		//Denna metoden behöver städas
-		Graphics2D g2 = (Graphics2D)g;
+
+
+		Graphics2D g2 = (Graphics2D)g; //Skapa grafikobjekt
+		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+				RenderingHints.VALUE_ANTIALIAS_ON);
+		Font font = new Font("Roboto", Font.PLAIN, 36);
+		g2.setFont(font);
+		FontMetrics fm = g2.getFontMetrics();
+		int fontHeight = fm.getAscent();
+		int biggestBox = 0; //Biggest box needed to align text nicely
 		
-		g2.fillRect(500, 500, 50, 50);
 		
-		int numOfBoxes = boxes.size();
-		double allBoxesHeight = 0.0;
-		
+		int numOfBoxes = boxes.size(); //Antal boxar
+		double allBoxesHeight = 0.0;   //Initsiera variablen för alla boxars höjd. Måste vara en double.
+
+		//Räkna ihop höjden, hitta största boxens storlek
 		for (GraphBox box : boxes) {
-			allBoxesHeight += (box.size + box.grow + box.votes);
+			int temp = (box.size + box.grow + box.votes);
+			allBoxesHeight += temp;
+			if (temp > biggestBox) biggestBox = temp;
 		}
 		
-		if( allBoxesHeight > graphHeight ){
-			//Boxes in percent
+		int nextY = yFloor; //Startpositionen för boxar, nerifrån
+		
+		//Loopa igenom alla boxar
+		for (GraphBox box : boxes) {
+			int sizeT = box.size + box.grow + box.votes; //Räkna ihop den totala storleken av en box.
 			
-			int nextY = yFloor;
-			for (GraphBox box : boxes) {
-				
-				//Total size of the box
-				int sizeT = box.size + box.grow + box.votes;
-				
-				//One box in percent
-				double percent = sizeT / allBoxesHeight;
-				
-				//Box size in percent converted to box size relative to the max height
-				sizeT = (int) Math.floor(percent*graphHeight);
-				
-				g2.setColor(box.color);
-				g2.fillRect(xAlign - (sizeT/2), nextY-sizeT, sizeT, sizeT);
-				nextY -= (sizeT);
+			//Om boxarnas höjd tillsamman är över den tillåtna storleken, räkna om boxarnsas induviduella storlek till procent.
+			if(allBoxesHeight > graphHeight){
+				double percent = sizeT / allBoxesHeight; //One box in percent.
+				sizeT = (int) Math.floor(percent*graphHeight); //Box size in percent converted to box size relative to the max height.
 			}
 			
-		}else{
-			//Boxes in px
+			g2.setColor(box.color); //Ställ in boxens färg.
+			g2.fillRect(xAlign - (sizeT/2), nextY-sizeT, sizeT, sizeT); //Rita ut boxen centrerat i x-led, med botten som utgångspunkt i y-led.
+			g2.drawString(box.awnser, (xAlign + (biggestBox/2) + 20), nextY-(sizeT/2)+(fontHeight/2)); //TODO: Svarsalternativen och antal röster
 			
-			int nextY = yFloor;
-			for (GraphBox box : boxes) {
-				int sizeT = box.size + box.grow + box.votes;
-				g2.setColor(box.color);
-				g2.fillRect(xAlign - (sizeT/2), nextY-sizeT, sizeT, sizeT);
-				nextY -= (sizeT);
-			}
+			nextY -= (sizeT); //Ta bort boxens storlek från y värdet så att nästa box hamnar ovan på.
 		}
 	}
-	
+
 	@Override
 	public void updateData(FirebaseData data) {
 		// TODO Auto-generated method stub
 		fbData = data;
-		
+
 		//Stoppa in storlekarna i rutorna
-		
+
 		boxes.get(0).update((int) (fbData.getVote1()*2));
 		boxes.get(1).update((int) (fbData.getVote2()*2));
 		boxes.get(2).update((int) (fbData.getVote3()*2));
-		
+
 		myLabel.setText(fbData.getQuestion());
 		repaint();
 	}
-	
+
 	ActionListener taskPerformer = new ActionListener() {
 		public void actionPerformed(ActionEvent evt) {
 			if(boxes.size() == 0){
@@ -126,7 +127,7 @@ public class VerticalBoxes extends JPanel implements ThemeInterface {
 			}
 		}
 	};
-	
+
 	class GraphBox{
 		//xPos is centered, yPos is on the top
 		int size = 50; //Initial size
@@ -135,7 +136,8 @@ public class VerticalBoxes extends JPanel implements ThemeInterface {
 		int yPos;
 		Color color;
 		int votes;
-		
+		String awnser = "Placeholder awnswer.";
+
 		GraphBox(Color color){
 			this.xPos = xAlign - (size/2);
 			this.color = color;
@@ -147,6 +149,6 @@ public class VerticalBoxes extends JPanel implements ThemeInterface {
 		void update(int votes){
 			this.votes = votes*2;
 		}
-		
+
 	}
 }
