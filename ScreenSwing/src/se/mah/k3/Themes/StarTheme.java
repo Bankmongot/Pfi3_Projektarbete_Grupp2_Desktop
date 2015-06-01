c9608ceda12f1324b167eb5e59dbc253516714e8
EmergencyBattle <emergencybattle@gmail.com>
2015-06-01 12:34:50 +0200

    cries a lot

--- ScreenSwing/src/se/mah/k3/Themes/StarTheme.java
+++ ScreenSwing/src/se/mah/k3/Themes/StarTheme.java
@@ -2,6 +2,9 @@ package se.mah.k3.Themes;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.Toolkit;
 
 import javax.swing.JLabel;
@@ -10,6 +13,7 @@ import javax.swing.JPanel;
 import se.mah.k3.Constants;
 import se.mah.k3.FirebaseData;
 import se.mah.k3.ThemeInterface;
+import se.mah.k3.Themes.VerticalBoxes.GraphBox;
 
 import javax.swing.ImageIcon;
 
@@ -19,6 +23,8 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 import java.awt.Font;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StarTheme extends JPanel implements ThemeInterface {
 
@@ -26,10 +32,12 @@ public class StarTheme extends JPanel implements ThemeInterface {
 	JLabel lblQuestionHere, lblAlt1, lblResult1;
 	private JTextField txtQuestionHere;
 	
+	List<StarCreate> stars = new ArrayList<StarCreate>();
+	
 	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 	double width = screenSize.getWidth();
 	double height = screenSize.getHeight();
-	
+	Image starImage;
 	
 	
 	public StarTheme(){
@@ -38,8 +46,16 @@ public class StarTheme extends JPanel implements ThemeInterface {
 		setMinimumSize (new Dimension(1024, 768));
 		System.out.println("Created StarPanel");
 		setLayout(null);
-		
+		starImage = Toolkit.getDefaultToolkit().getImage(VerticalBoxes.class.getResource("/images/Star-1.png"));
 
+		stars.add(new StarCreate((int) (width/8*1-150), 400));
+		stars.add(new StarCreate((int) (width/8*2), 250));
+		stars.add(new StarCreate((int) (width/8*3-200), 650));
+		stars.add(new StarCreate((int) (width/8*4-200), 400));
+		stars.add(new StarCreate((int) (width/8*5-200), 600));
+		stars.add(new StarCreate((int) (width/8*6-200), 250));
+		stars.add(new StarCreate((int) (width/8*7-200), 550));
+		
 		
 		//JLabel lblQuestionHere = new JLabel(fbData.getQuestion());
 		JLabel lblQuestionHere = new JLabel("Question?");
@@ -138,44 +154,88 @@ public class StarTheme extends JPanel implements ThemeInterface {
 		lblResult7.setFont(new Font("Roboto", Font.BOLD, 15));
 		lblResult7.setBounds((int) (width/8*7-100), 177, 46, 14);
 		add(lblResult7);
-		
-		/*JLabel lblBackground = new JLabel("New label");
-		lblBackground.setIcon(new ImageIcon(StarTheme.class.getResource("/images/NightSkyBackground.png")));
-		lblBackground.setBounds(0, 0, 1920, 1080);
-		add(lblBackground);*/
-
-		/*lblNewLabel_1 = new JLabel("New label");
-		lblNewLabel_1.setBounds(161, 81, 175, 16);
-		lblNewLabel_1.setText("Label 1");
-		add(lblNewLabel_1);
-		lblNewLabel_3.setText(fbData.getQuestion());*/
-		
 	}
 	
 	protected void paintComponent(Graphics g) {
 		 //g.setColor(getBackground()); // x , y , w, h , let stay null
 		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D)g; //Create the graphics2D object we'll use for drawing
+
 		 g.drawImage(Toolkit.getDefaultToolkit().getImage(StarTheme.class.getResource("/images/NightSkyBackground.png")), 0,0, (int) width, (int) height, null);
-		 g.drawImage(Toolkit.getDefaultToolkit().getImage(StarTheme.class.getResource("/images/Star-1.png")), (int) (width/8*1-150), 400, 300, 300, null);
+		/* g.drawImage(Toolkit.getDefaultToolkit().getImage(StarTheme.class.getResource("/images/Star-1.png")), (int) (width/8*1-150), 400, 300, 300, null);
 		 g.drawImage(Toolkit.getDefaultToolkit().getImage(StarTheme.class.getResource("/images/Star-2.png")), (int) (width/8*2), 250, 300, 300, null);
 		 g.drawImage(Toolkit.getDefaultToolkit().getImage(StarTheme.class.getResource("/images/Star-3.png")), (int) (width/8*3-200), 650, 300, 300, null);
 		 g.drawImage(Toolkit.getDefaultToolkit().getImage(StarTheme.class.getResource("/images/Star-4.png")), (int) (width/8*4-200), 400, 300, 300, null);
 		 g.drawImage(Toolkit.getDefaultToolkit().getImage(StarTheme.class.getResource("/images/Star-5.png")), (int) (width/8*5-200), 600, 300, 300, null);
 		 g.drawImage(Toolkit.getDefaultToolkit().getImage(StarTheme.class.getResource("/images/Star-6.png")), (int) (width/8*6-200), 250, 300, 300, null);
-		 g.drawImage(Toolkit.getDefaultToolkit().getImage(StarTheme.class.getResource("/images/Star-7.png")), (int) (width/8*7-200), 550, 300, 300, null);
+		 g.drawImage(Toolkit.getDefaultToolkit().getImage(StarTheme.class.getResource("/images/Star-7.png")), (int) (width/8*7-200), 550, 300, 300, null);*/
+		 
+		 int totalVotes = 0;
+		 
+			for (StarCreate star : stars) {
+				int votes = star.votes;
+				totalVotes += votes;
+			}
+		 
+		 for (StarCreate star : stars) {
+				if (star.votes > 0){
+					int size = star.votes; //Box size.
+					double percent = size / totalVotes; //One box in percent.
+					size = (int) Math.floor(percent*300); //Box size in percent converted to box size relative to the max height.
+
+					g2.drawImage(starImage, star.xPos, star.yPos, size, size, this); //Draw box, centered on xAlign with the bottom as origin for the y coordinate.
+					/*g2.drawString(box.answer, (int)(xAlign + (biggestBox/2)) + 50 , (nextY-(size/2)+(fontHeight/2))-5); // Answer, aligned by the biggest box
+					g2.drawString((int)Math.floor(percent*100)+"%", (int)((xAlign - (biggestBox/2)) - (barOffset - 40)), (nextY-(size/2)+(fontHeight/2))-5); //Votes in %, -||-*/
+
+					}
+			}
 		 
       //g.fillRect(0, 0, getWidth(), getHeight());
 		this.repaint();
 	}
 	
+	
+	
 	@Override
 	public void updateData(FirebaseData data) {
-		// TODO Auto-generated method stub
-		/*fbData = data;
+		fbData = data;
 		lblQuestionHere.setText(fbData.getQuestion());
-		*/
+		
+		ArrayList<String> answers = fbData.getAnswers();
+		ArrayList<Integer> votes = fbData.getVotes();
+		
+		//Add data to boxes
+		System.out.println("Boxes"+ stars.size());
+		System.out.println("Answers"+ answers.size());
+		System.out.println("Votes"+ votes.size());
+
+		for(int i = 0; i<answers.size(); i++){
+			stars.get(i).update((int) (votes.get(i)), answers.get(i));
+			System.out.println("Updated box");
+		}
+
+		
 		repaint();
 	} 
 	
+	class StarCreate{
+		int votes = 0;
+		String answer;
+		int xPos;
+		int yPos;
+
+		StarCreate(int x, int y)
+		{ 
+			this.xPos = x;
+			this.yPos = y;
+		}
+		void update(int votes, String answer){ 
+			this.votes = votes;
+			this.answer = answer;
+
+		}
+
+
+		}
 
 }
