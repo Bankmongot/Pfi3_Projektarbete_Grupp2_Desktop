6353b4ae9697e932449f6dc54459e7b7d60b5d34
Mcni91 <mcni.91@gmail.com>
2015-06-03 15:23:14 +0200

    Bottles done

--- ScreenSwing/src/se/mah/k3/Themes/BottleTheme.java
+++ ScreenSwing/src/se/mah/k3/Themes/BottleTheme.java
@@ -33,11 +33,13 @@ public class BottleTheme extends JPanel implements ThemeInterface {
 	JLabel myLabel;
 	ArrayList<Bottle> bottles = new ArrayList<Bottle>();
 	ArrayList<Color> colors = new ArrayList<Color>();
+	ArrayList<Image> labels = new ArrayList<Image>();
 	Image bottle1;
 	Image bottle2;
 	Image bcgr;
 	Image table;
 	Image liquidGradient;
+	Image labelGradient;
 
 
 
@@ -53,13 +55,21 @@ public class BottleTheme extends JPanel implements ThemeInterface {
 		bcgr = Toolkit.getDefaultToolkit().getImage(VerticalBoxes.class.getResource("/images/bottles.png"));
 		table = Toolkit.getDefaultToolkit().getImage(VerticalBoxes.class.getResource("/images/table.png"));
 		liquidGradient = Toolkit.getDefaultToolkit().getImage(VerticalBoxes.class.getResource("/images/bottleGradient.png"));
-		
+		labelGradient = Toolkit.getDefaultToolkit().getImage(VerticalBoxes.class.getResource("/images/bottleLabelGradient.png"));
+
 		colors.add(new Color(Integer.parseInt("fd8a85",16)));
 		colors.add(new Color(Integer.parseInt("feba07",16)));
 		colors.add(new Color(Integer.parseInt("e5dbcd",16)));
 		colors.add(new Color(Integer.parseInt("ff8f01",16)));
 		colors.add(new Color(Integer.parseInt("01c5f7",16)));
 		colors.add(new Color(Integer.parseInt("eff277",16)));
+		
+		labels.add(Toolkit.getDefaultToolkit().getImage(VerticalBoxes.class.getResource("/images/labelone.png")));
+		labels.add(Toolkit.getDefaultToolkit().getImage(VerticalBoxes.class.getResource("/images/labeltwo.png")));
+		labels.add(Toolkit.getDefaultToolkit().getImage(VerticalBoxes.class.getResource("/images/labelthree.png")));
+		labels.add(Toolkit.getDefaultToolkit().getImage(VerticalBoxes.class.getResource("/images/labelfour.png")));
+		labels.add(Toolkit.getDefaultToolkit().getImage(VerticalBoxes.class.getResource("/images/labelfive.png")));
+		labels.add(Toolkit.getDefaultToolkit().getImage(VerticalBoxes.class.getResource("/images/labelsix.png")));
 
 
 		//Question
@@ -76,36 +86,36 @@ public class BottleTheme extends JPanel implements ThemeInterface {
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
-		
+
+
 		//Font fontQuestuion = new Font("Roboto", Font.PLAIN, 50);
 		//Font fontAnswer = new Font("Roboto", Font.PLAIN, 30);
-		
+
 		Graphics2D g2 = (Graphics2D)g; //Create the graphics2D object we'll use for drawing
 		Graphics2D g2A = (Graphics2D) g.create();
-		
+
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); //Anti aliasing, mainly for text
-		
+
 		float alpha = 0.8f;
-        AlphaComposite alcom = AlphaComposite.getInstance(
-                AlphaComposite.SRC_OVER, alpha);
-        g2A.setComposite(alcom);
-		
+		AlphaComposite alcom = AlphaComposite.getInstance(
+				AlphaComposite.SRC_OVER, alpha);
+		g2A.setComposite(alcom);
+
 		//Determining window-size on every repaint enables smooth re-scaling
 		Dimension dim = this.getSize();
 		double windowWidth = dim.getWidth();
 		double windowHeight = dim.getHeight();
-		
+
 		//Set graph width to two thirds of the display
 		double graphWidth = windowWidth-(windowWidth/3);
-		
+
 		//Calculate scale with 1920z1080 as 1:1
 		double scaleWindow = (windowWidth / 1920);
 		double scale = (graphWidth / 1920);
-		
+
 		//Set maximum table width to 3/4 of the graph width
 		double tableWidth = graphWidth-(graphWidth / 4);
-		
+
 		//Loop through bottles to get total amount of votes (and how many bottles there are while we're at it) 
 		int numOfBottles = 0;
 		double totalVotes = 0;
@@ -113,60 +123,78 @@ public class BottleTheme extends JPanel implements ThemeInterface {
 			totalVotes += bottle.votes;
 			numOfBottles++;
 		}
-		
+
 		//Reduce the table width depending on number of answers. 
 		tableWidth = (tableWidth/6) * numOfBottles; 
-		
+
 		//Center the virtual table the bottles are drawn on.
 		int xStart = (int) ((graphWidth - tableWidth)/2);
-		
+
 		//Calculate where to put bottles for them to be on the table
 		int tableY =  (int)(windowHeight-((graphWidth/1920)*1080)+5);
-		
+
 		//Calculate width and height of the bottles in relation to scale. The numbers here are the image dimensions in pixels.
 		int bottleWidth  = (int)(219*scale);
 		int bottleHeight = (int)(709*scale);
-		
+
 		//Calculate width of each bottle including space between them
 		int spaceBetween = (int) (((tableWidth - (bottleWidth*numOfBottles))/numOfBottles)+bottleWidth);
-		
-		int fontSize = (int) (40*scaleWindow);
+
+		Font fontTitle  = new Font("Roboto", Font.PLAIN, (int) (50*scaleWindow));
+
+		int fontSize = (int)(40*scaleWindow);
 		Font fontAnswer = new Font("Roboto", Font.PLAIN, fontSize);
-		
+
+		String question = fbData.getQuestion();
+
 		//DRAWING :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-		
-		g2.setFont(fontAnswer);
-		
+
+
 		//Draw background image
 		g2.drawImage(bcgr, 0, 0, (int) windowWidth, (int) windowHeight, this);
 		//Draw table
 		g2.drawImage(table, 0, tableY, (int) graphWidth, (int)((graphWidth/1920)*1080), this);
-		
+
+		g2.setFont(fontTitle);
+		FontMetrics metrics = g2.getFontMetrics(fontTitle);
+		int titleWidth = metrics.stringWidth(question);
+		int titleXpos = (int)((windowWidth - titleWidth)/2.0);
+
+		g2.drawString(question, titleXpos, tableY-(fontSize*2)); //Answer, aligned by the biggest box
+
+		g2.setFont(fontAnswer);
+
 		//Drawing loop.
 		int bottleCount = 0;
 		for (Bottle bottle : bottles){
-			
+
 			if (bottle.votes > 0) {
 				double percent = bottle.votes / totalVotes; //One bar in percent.
-				int barHeight = (int) Math.floor((percent*(bottleHeight/2))-(10*scale)); //Box size in percent converted to box size relative to the max height.
+				int barHeight = (int) Math.floor((percent*(bottleHeight-(bottleHeight/3.0)))-(60*scale)); //Box size in percent converted to box size relative to the max height.
 				int bottomOfBottle = (int)((tableY+(50*scale))+bottleHeight);
-				
+
 				int barXActual = (int) (((bottleCount*spaceBetween)+xStart) + (15*scale));
 				int barYActual = (int)(bottomOfBottle-barHeight-(30*scale));
 				int barWActual = (int)(bottleWidth-(bottleWidth/8.0));
 				
-				g2A.setColor(bottle.color);
-				g2A.fillRect((int) (((bottleCount*spaceBetween)+xStart) + (15*scale)), (int)(bottomOfBottle-barHeight-(30*scale)), (int)(bottleWidth-(bottleWidth/8.0)), barHeight);
-				g2A.drawImage(liquidGradient, barXActual, barYActual, barWActual, barHeight, this);
+				if(barHeight > 0){
+					g2A.setColor(bottle.color);
+					g2A.fillRect((int) (((bottleCount*spaceBetween)+xStart) + (15*scale)), (int)(bottomOfBottle-barHeight-(30*scale)), (int)(bottleWidth-(bottleWidth/8.0)), barHeight);
+					g2A.drawImage(liquidGradient, barXActual, barYActual, barWActual, barHeight, this);
+				}
 			}
-			
+			int labelWidth = (int)(bottleWidth-(bottleWidth/3.0));
+			int bottleX = ((bottleCount*spaceBetween)+xStart);
+			int bottleY = (int)(tableY+(50*scale));
 			//Draw bottle
-			g2.drawImage(bottle.image, ((bottleCount*spaceBetween)+xStart), (int)(tableY+(50*scale)), bottleWidth, bottleHeight, this); //Draw bottle, aligned with the table.
+			g2.drawImage(bottle.image, bottleX, bottleY, bottleWidth, bottleHeight, this); //Draw bottle, aligned with the table.
+			g2.drawImage(bottle.label, (int)(bottleX+((bottleWidth-labelWidth)/2.0)), (int)(bottleY+(bottleHeight/2.0)), labelWidth, (int)(labelWidth-(labelWidth/2.0)), this); //Draw bottle, aligned with the table.
+			g2A.drawImage(labelGradient, (int)(bottleX+((bottleWidth-labelWidth)/2.0)),  (int)(bottleY+(bottleHeight/2.0)), labelWidth, (int)(labelWidth-(labelWidth/2.0)), this);
 			//Draw text
-			g2.drawString(bottle.answer, (int) graphWidth, (int)((fontSize*1.5)*bottleCount)+tableY); //Answer, aligned by the biggest box
+			g2.drawString((bottleCount + 1) + ". " + bottle.answer + " (" + bottle.votes + ")", (int) graphWidth, (int)((fontSize*2.0)*bottleCount)+tableY+fontSize); //Answer, aligned by the biggest box
 			bottleCount++;
 		}
-		//g2.dispose();
+		g2.dispose();
 	}
 
 
@@ -179,10 +207,10 @@ public class BottleTheme extends JPanel implements ThemeInterface {
 		ArrayList<Integer> votes = fbData.getVotes();
 
 		System.out.println("Vertical boxes, updateData(). Data received: " + fbData.getAnswers() + " " + fbData.getVotes());
-		
+
 		for(int i = bottles.size(); i<answers.size(); i++){
-			if(Math.random() > 0.5) bottles.add(new Bottle(bottle1, colors.get(i)));
-			else bottles.add(new Bottle(bottle2, colors.get(i)));
+			if(Math.random() > 0.5) bottles.add(new Bottle(bottle1, colors.get(i), labels.get(i)));
+			else bottles.add(new Bottle(bottle2, colors.get(i), labels.get(i)));
 			System.out.println("Added box");
 		}
 
@@ -200,8 +228,9 @@ public class BottleTheme extends JPanel implements ThemeInterface {
 		String answer;
 		Image image;
 		Color color;
+		Image label;
 
-		Bottle(Image img, Color c){ this.image = img; this.color = c;}
+		Bottle(Image img, Color c, Image l){ this.image = img; this.color = c; this.label = l;}
 		void update(int votes, String answer){ 
 			this.votes = votes;
 			this.answer = answer;
